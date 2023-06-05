78d60dae3d7ae31b40bfdb1592cc39ea659f3e57
AnnMLin <smilemarg@gmail.com>
2023-06-04 20:36:03 -0400

    Add avatar and app css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div>Hello world!</div>
+      <div id="namespace">
+        <img src="/avatar.png" alt="avatar" width="96" height="96"/>
+        <div>Ann Afloat</div>
+      </div>
+
     </div>
   );
 }
