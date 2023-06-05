f81b5b519a1cfc254e91ae9ed2480cff1cb6fc0d
AnnMLin <smilemarg@gmail.com>
2023-06-05 01:23:50 -0400

    MVP fix image path

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <div id="namespace">
-        <img src="/avatar.png" alt="avatar" width="96" height="96"/>
+        <img src={process.env.PUBLIC_URL + "/avatar.png"} alt="avatar" width="96" height="96"/>
         <div id="name">Ann Afloat</div>
       </div>
       <Links />
