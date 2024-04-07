d1ddc825ed628af9879f83ef0b088abd29dcbdc0
AnnAfloatX <smilemarg@gmail.com>
2024-04-06 23:02:17 -0400

    avatar style change

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,13 @@ function App() {
   return (
     <div className="App">
       <div id="namespace">
-        <img src={process.env.PUBLIC_URL + "/avatar.png"} alt="avatar" width="96" height="96"/>
+        <div id="avatar-wrap">
+          <span class="helper"></span>
+          <img src={process.env.PUBLIC_URL + "/avatar.png"} alt="avatar"/>
+          <svg viewBox="0 0 110 110">
+            <circle cx="55" cy="55" r="53" stroke="white" fill="none" stroke-width="2"/>
+          </svg>
+        </div>
         <div id="name">Ann Afloat</div>
       </div>
       <Links />
